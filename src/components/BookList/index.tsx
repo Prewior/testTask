62ce05af3e1fd19ef9f704c91183b20289112e9b
Vitalii K <Vitalii.Kolodribskyi@devoxsoftware.com>
2023-12-06 19:05:51 -0700

    Build.

--- src/components/BookList/index.tsx
+++ src/components/BookList/index.tsx
@@ -1,4 +1,3 @@
-// src/components/BookList.tsx
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../redux/store';
